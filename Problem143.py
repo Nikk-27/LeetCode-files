0e1207260175efb3acc9f5cacaeec85b3c856ba4
Nikk-27 <nktllwn@gmail.com>
2024-06-29 19:39:05 -0700

    Problem 143 Reorder LL ***

--- Problem143.py
+++ Problem143.py
@@ -9,11 +9,46 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:          # TC : O(N), SC : O(1)
+    def reorderList(self, head: Optional[ListNode]) -> Optional[ListNode]:          # TC : O(N), SC : O(1)
         """
         Do not return anything, modify head in-place instead.
         """
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
+        #reverse second half
+        second = slow.next
+        prev = slow.next = None
+       
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        
+        # Debug: Print the two halves
+        
+        # self.printList(head, "First half after splitting and reversing the second half:")
+        # self.printList(prev, "Second half reversed:")
+
+        #merge two halfs
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
+        return head
+    
+    def printList(self, node: ListNode, msg: str) -> None:
+        print(msg)
+        while node:
+            print(node.val, end=" -> ")
+            node = node.next
+        print("None")
 
 
 # Utility function to convert a Python list to a linked list
