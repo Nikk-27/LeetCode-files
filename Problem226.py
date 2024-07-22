bb1eb31d3ed475ea2a75003ba5a813b4eab704bd
Nikk-27 <nktllwn@gmail.com>
2024-07-21 17:13:09 -0700

    Problem 226 Binary tree reversal/ invert/ mirror BT

--- Problem226.py
+++ Problem226.py
@@ -74,3 +74,31 @@ print(print_tree(inverted_root))
 
 
 # recursive method uses depth-first traversal to swap nodes, while the iterative method uses breadth-first traversal (level order) with a queue to swap nodes.
+
+'''
+LEVEL ORDER
+
+TreeNode invertTree(TreeNode root) {
+  if (root == null) {
+    return null;
+  }
+
+  final Queue<TreeNode> queue = new LinkedList<>();
+  queue.add(root);
+
+  while (!queue.isEmpty()) {
+    final TreeNode node = queue.poll();
+
+    // Swap nodes
+    final TreeNode temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    // Add left and right of this node to the queue
+    if (node.left != null) queue.add(node.left);
+
+    if (node.right != null) queue.add(node.right);
+  }
+  return root;
+}
+'''
