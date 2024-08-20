9ad27cd87987abedb45ab26ef133d96a68b2f6ee
Nikk-27 <nktllwn@gmail.com>
2024-08-20 01:05:28 -0700

    Problem 98 valid Binary search tree

---
+++ Problem98.py
@@ -0,0 +1,42 @@
+#98 Validate Binary Search Tree
+
+from typing import Optional, List
+from collections import deque
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+
+# Utility function to create a binary tree from a list of values
+def create_tree(values: List[Optional[int]]) -> TreeNode:
+    if not values:
+        return None
+    
+    def insert_level_order(arr, root, i, n):
+        if i < n and arr[i] is not None:
+            temp = TreeNode(arr[i])
+            root = temp
+            
+            # Insert left child
+            root.left = insert_level_order(arr, root.left, 2 * i + 1, n)
+            
+            # Insert right child
+            root.right = insert_level_order(arr, root.right, 2 * i + 2, n)
+        
+        return root
+    
+    return insert_level_order(values, None, 0, len(values))
+
+
+root = [2,None,4,10,8,None,None,4]
+root_tree = create_tree(root)
+solution = Solution()
+print(solution.isValidBST(root_tree))
