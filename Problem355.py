5132ac808bb1f943c9ea268c09a1fb8da60c8b8c
Nikk-27 <nktllwn@gmail.com>
2024-10-01 14:53:36 -0700

    P355 design twitter

--- Problem355.py
+++ Problem355.py
@@ -1,24 +1,112 @@
 #355 Design Twitter
+        
+import heapq
+from collections import defaultdict, deque
 
-from typing import List
+class Tweet:
+    def __init__(self, time, tweetId):
+        self.time = time
+        self.tweetId = tweetId
+
+    def __lt__(self, other):
+        return self.time > other.time  # Max heap in Python uses min heap by default, so reverse the comparison
+
+class User:
+    def __init__(self, userId):
+        self.userId = userId
+        self.followers = set()
+        self.tweets = deque()  # Use deque for efficient head insertion
+
+    def add_tweet(self, tweet):
+        self.tweets.appendleft(tweet)  # Add tweet at the head
+
+    def add_follower(self, followeeId):
+        self.followers.add(followeeId)
+
+    def remove_follower(self, followeeId):
+        self.followers.discard(followeeId)
 
 class Twitter:
-
     def __init__(self):
-        
+        self.user_map = defaultdict(User)  # Automatically creates User if not present
+        self.time_counter = 0
 
-    def postTweet(self, userId: int, tweetId: int) -> None:
-        
+    def post_tweet(self, userId, tweetId):
+        self.time_counter += 1
+        if userId not in self.user_map:
+            self.user_map[userId] = User(userId)
+        user = self.user_map[userId]
+        user.add_tweet(Tweet(self.time_counter, tweetId))
 
-    def getNewsFeed(self, userId: int) -> List[int]:
+    def get_news_feed(self, userId):
+        if userId not in self.user_map:
+            return []
         
+        pq = []
+        user = self.user_map[userId]
 
-    def follow(self, followerId: int, followeeId: int) -> None:
+        # Add tweets of followers
+        for followerId in user.followers:
+            count = 0
+            for tweet in self.user_map[followerId].tweets:
+                heapq.heappush(pq, tweet)
+                count += 1
+                if count >= 10:
+                    break
+
+        # Add own tweets
+        count = 0
+        for tweet in user.tweets:
+            heapq.heappush(pq, tweet)
+            count += 1
+            if count >= 10:
+                break
+
+        # Get the top 10 most recent tweets
+        res = []
+        while pq and len(res) < 10:
+            tweet = heapq.heappop(pq)
+            res.append(tweet.tweetId)
         
+        return res
 
-    def unfollow(self, followerId: int, followeeId: int) -> None:
-        
+    def follow(self, followerId, followeeId):
+        if followerId not in self.user_map:
+            self.user_map[followerId] = User(followerId)
+        if followeeId not in self.user_map:
+            self.user_map[followeeId] = User(followeeId)
 
+        self.user_map[followerId].add_follower(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        if followerId not in self.user_map or followeeId not in self.user_map:
+            return
+        self.user_map[followerId].remove_follower(followeeId)
+
+# Process the commands
+results = []
+
+commands = ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+values = [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+
+for command, value in zip(commands, values):
+    if command == "Twitter":
+        twitter = Twitter()
+        results.append(None)  # Initialization does not return a value
+    elif command == "postTweet":
+        twitter.post_tweet(value[0], value[1])
+        results.append(None)  # Put does not return a value
+    elif command == "getNewsFeed":
+        result_value = twitter.get_news_feed(value[0])
+        results.append(result_value)  # get does return a value to corresponding key
+    elif command == "follow":
+        twitter.follow(value[0], value[1])
+        results.append(None)  # Put does not return a value
+    elif command == "unfollow":
+        twitter.unfollow(value[0], value[1])
+        results.append(None)  # Put does not return a value
+
+print(results)
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
@@ -26,7 +114,6 @@ class Twitter:
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
-
 '''
 class Tweet implements Comparable<Tweet>{
     int time;
