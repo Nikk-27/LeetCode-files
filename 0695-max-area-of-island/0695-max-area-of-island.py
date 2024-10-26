5c10f5f085eb4abb1b251d9b1ff2c9f8ff0e8fe3
Nikita Lalwani <nktllwn@gmail.com>
2024-10-26 00:37:12 -0700

    Time: 21 ms (96.06%), Space: 17.2 MB (55.82%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,34 @@
+class Solution:
+    def __init__(self):
+        self.sum = 0
+
+    def dfs(self, row, col, grid):
+        if (row < 0 or col < 0 or 
+        row >= len(grid) or col >= len(grid[0]) or 
+        grid[row][col] != 1):
+            return
+
+        grid[row][col] = "$"
+        self.sum += 1
+
+        # Explore all four directions (up, down, left, right)
+        self.dfs(row + 1, col, grid)
+        self.dfs(row - 1, col, grid)
+        self.dfs(row, col + 1, grid)
+        self.dfs(row, col - 1, grid)
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        count = 0
+        answer = []
+
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 1:
+                    count += 1
+                    self.sum = 0
+                    self.dfs(i, j, grid)
+                    print(self.sum)
+                    answer.append(self.sum)
+        return max(answer) if answer else 0
