ee06f15ca2258a6b51a50afdcd89a5321d144831
Nikita Lalwani <nktllwn@gmail.com>
2024-12-02 11:31:09 -0800

    Time: 90 ms (5.3%), Space: 19.3 MB (99.55%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_dict = {}
+        
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            
+            if sorted_word in anagram_dict:
+                anagram_dict[sorted_word].append(word)
+            else:
+                anagram_dict[sorted_word] = [word]
+        
+        grouped_anagrams = list(anagram_dict.values())
+        return grouped_anagrams
