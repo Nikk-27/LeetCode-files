7faa913bca1b402b78c142af1f611577bf29cbc8
Nikita Lalwani <nktllwn@gmail.com>
2025-01-21 01:46:56 -0800

    Time: 3 ms (92.16%), Space: 21.2 MB (56.74%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count_n = {}
+        for n in nums:
+            count_n[n] = 1 + count_n.get(n, 0)
+        heap = []
+        res = []
+
+        for n, cnt in count_n.items():
+            heapq.heappush(heap, [cnt, n])
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        for n in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+
