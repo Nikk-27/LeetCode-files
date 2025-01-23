6563116239050f22823eefa30936b2457f9aa1f4
Nikita Lalwani <nktllwn@gmail.com>
2025-01-22 17:47:17 -0800

    Time: 8 ms (99.28%), Space: 18.1 MB (61.13%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,23 +1,62 @@
+#### BFS ####
+
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != 1):
-                return 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        def bfs(r, c, q, sum):
             grid[r][c] = 0
+            sum += 1
+            q.append((r,c))
 
-            return (1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1))
+            while q:
+                rr, cc = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = rr+dr, cc+dc
+                    if (nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] != 1):
+                        continue
+                    grid[nr][nc] = 0
+                    sum += 1
+                    q.append((nr, nc))
+            return sum
 
         ROWS = len(grid)
         COLS = len(grid[0])
+        q = deque()
 
         sum = 0
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    sum = max(dfs(r, c), sum)
+                    sum = max(bfs(r, c, q, 0), sum)
         return sum
+
+# Time complexity: O(m∗n)
+# Space complexity: O(m∗n)
+
+
+#### DFS ####
+
+# class Solution:
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+#         def dfs(r, c):
+#             if (r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != 1):
+#                 return 0
+#             grid[r][c] = 0
+
+#             return (1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1))
+
+#         ROWS = len(grid)
+#         COLS = len(grid[0])
+
+#         sum = 0
+
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if grid[r][c] == 1:
+#                     sum = max(dfs(r, c), sum)
+#         return sum
 
 
 
