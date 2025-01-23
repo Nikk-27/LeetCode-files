c137f5d24f93752d70430458f6fcb2bfb8d7cd92
Nikita Lalwani <nktllwn@gmail.com>
2025-01-22 16:23:40 -0800

    Time: 241 ms (70.75%), Space: 20.2 MB (51.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,20 @@
 class Solution:
-
-    def dfs(self, row, col, grid):
-
-        if (row < 0 or col < 0 or 
-        row >= len(grid) or col >= len(grid[0]) or 
-        grid[row][col] != '1'):
-            return
-
-        grid[row][col] = "$"
-
-        # Explore all four directions (up, down, left, right)
-        self.dfs(row + 1, col, grid)
-        self.dfs(row - 1, col, grid)
-        self.dfs(row, col + 1, grid)
-        self.dfs(row, col - 1, grid)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != "1"):
+                return
+            grid[r][c] = "0"
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        ROWS = len(grid)
+        COLS = len(grid[0])
         count = 0
-
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1':
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
                     count += 1
-                    self.dfs(i, j, grid)
+                    dfs(r, c)
         return count
-
-
-'''
-Time Complexity:
-DFS Traversal: Each cell in the grid is visited once. The DFS explores in four directions (up, down, left, right) but each cell is processed at most once, so the time complexity is proportional to the number of cells in the grid.
-If the grid has m rows and n columns, the time complexity is O(m * n).
-
-Space Complexity:
-Recursive DFS Stack: The depth of the recursion can go as deep as the number of cells in the worst case. Therefore, the space used by the recursion stack is O(m * n) in the worst case.
-'''
-
