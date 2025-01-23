c2eb255c5b22252e2910c234695a93c78879ec59
Nikita Lalwani <nktllwn@gmail.com>
2025-01-22 19:15:33 -0800

    Time: 45 ms (37.32%), Space: 18.3 MB (24.04%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,27 +5,62 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-#### DFS ####
+
+#### BFS ####
+
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldtonew = {}
+        if not node:
+            return None
 
-        def dfs(node):
-            if node in oldtonew:
-                return oldtonew[node]
+        def bfs(start_node: 'Node') -> 'Node':
+            oldtonew = {}
+            q = deque([start_node])
 
-            copy_node = Node(node.val)
-            oldtonew[node] = copy_node
+            # Create the clone for the starting node
+            oldtonew[start_node] = Node(start_node.val)
 
-            for neighbour in node.neighbors:
-                copy_node.neighbors.append(dfs(neighbour))
+            while q:
+                cur = q.popleft()
 
-            return copy_node
+                for nei in cur.neighbors:
+                    # If the neighbor is not cloned yet, clone it
+                    if nei not in oldtonew:
+                        oldtonew[nei] = Node(nei.val)
+                        q.append(nei)
+                    # Add the cloned neighbor to the current node's clone
+                    oldtonew[cur].neighbors.append(oldtonew[nei])
+
+            return oldtonew[start_node]
+
+        return bfs(node)
+
+#TC = O(V+E)
+#SC = O(V) 
+
+
+#### DFS ####
+
+# from typing import Optional
+# class Solution:
+#     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+#         oldtonew = {}
+
+#         def dfs(node):
+#             if node in oldtonew:
+#                 return oldtonew[node]
+
+#             copy_node = Node(node.val)
+#             oldtonew[node] = copy_node
+
+#             for neighbour in node.neighbors:
+#                 copy_node.neighbors.append(dfs(neighbour))
+
+#             return copy_node
             
 
-        return dfs(node) if node else None
-
+        # return dfs(node) if node else None
 
 #TC = O(V+E)
 #SC = O(V)    
