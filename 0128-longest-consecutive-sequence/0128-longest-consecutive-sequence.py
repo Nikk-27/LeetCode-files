449ed93d8e99c765ddfedf23710cf65f8d3d65d7
Nikita Lalwani <nktllwn@gmail.com>
2025-02-03 23:13:30 -0800

    Time: 75 ms (33.18%), Space: 33.2 MB (90.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -15,5 +15,49 @@ class Solution:
         return longest
 
 
+# TC = O(N)
+# SC = O(N) this method doesnt alter our original array
+
+
+'''
+nums = [100,4,200,1,3,2]
+ 
+ we first do brute force:
+ 
+ longest = 0
+ for i in range(len(nums)):
+     cur = nums[i]
+     maxi = 1
+     while cur + 1 in nums:
+           cur += 1
+           maxi += 1
+     longest = max(longest, maxi)
+  return longest
+ 
+ this take TC = O(n^2) and SC = O(1)
+ 
+ now we do sort method
+ 
+ def longestConsecutive(self, nums: List[int]) -> int:
+        longest = 0
+        new_num = sorted(nums)
+        length = 0
+        small = float(-inf)
+         
+        for i in range(len(new_num)):
+            if new_num[i] - 1 == small:
+                small = new_num[i]
+                length += 1
+            if new_num[i] != small:
+                small = new_num[i]
+                length = 1
+            longest = max(length, longest)
+        return longest
+
+TC = O(n) + O(n)
+SC = O(1)
+
+
+'''
 
                 
