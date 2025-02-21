c35a2ca648d18753a515263a84f83dc0ba553a5a
Nikita Lalwani <nktllwn@gmail.com>
2025-02-20 18:09:55 -0800

    Time: 16 ms (72.95%), Space: 19.8 MB (41.58%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -1,3 +1,29 @@
+# TC = O(N)
+# SC = O(N)
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        # we try to reduce space complexity by taking one dict
+
+        count_first_last = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in count_first_last:
+                count_first_last[nums[i]] = [0, i, i]
+            count_first_last[nums[i]][0] += 1
+            count_first_last[nums[i]][2] = i
+
+        length = float('inf')
+
+        max_freq = max(count_first_last.values(), key=lambda x : x[0])[0] # this would give the list with highest freq so out of that list we need the first element becasue that is the highest freq
+
+        for i in count_first_last.values():
+            if max_freq == i[0]:
+                length = min(length, i[2] - i[1] + 1)
+
+        return length
+
+'''
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         length = float('inf')
@@ -17,7 +43,4 @@ class Solution:
 
         return length
 
-        
-
-
-        print(count1)
+'''
