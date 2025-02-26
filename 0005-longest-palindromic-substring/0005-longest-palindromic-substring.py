a45657a7b486c8a9936a84afed5e0898fecba4b5
Nikita Lalwani <nktllwn@gmail.com>
2025-02-25 17:54:50 -0800

    Time: 215 ms (93.13%), Space: 17.8 MB (61.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,35 @@
-# Bottom up way - Blueprint method
+# Expand Around Center
 # TC = O(N^2)
-# SC = O(N^2)
+# SC = O(1)
 
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
+        output_s = ""
         maxL = float('-inf')
-        idx = 0
-        t = [[False] * n for _ in range(n)]
+
+        if n <= 1:
+            return s
+        
+        def solve(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
 
         for i in range(n):
-            t[i][i] = True
-            maxL = 1
+            odd = solve(i, i)
+            even = solve(i, i+1)
 
-        for L in range(2, n+1):
-            for i in range(0, n-L+1):
-                j = i+L-1
+            if len(odd) > len(even) and len(odd) > maxL:
+                output_s = odd
+                maxL = len(odd)
+            elif len(odd) < len(even) and len(even) > maxL :
+                output_s = even
+                maxL = len(even)
+           
+        return output_s
 
-                if i+1 == j and s[i] == s[j]:
-                    t[i][j] = True
-                    maxL = 2
-                    idx = i
-                elif s[i] == s[j] and t[i+1][j-1]:
-                    t[i][j] = True
-                    if j-i+1 > maxL:
-                        maxL = j-i+1
-                    idx = i
-
-        return s[idx: idx+maxL]
-
-
-
+        
         
